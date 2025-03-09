da750812378f50155135c9a79b9cb9c1d1444b62
Anderson de Almeida <andersondev999@gmail.com>
2025-03-09 15:47:58 -0300

    Fim da Aula Prática 02

--- game_code/Consts.py
+++ game_code/Consts.py
@@ -1,3 +1,18 @@
 # DEFINIÇÃO DE CONSTANTES PARA O JOGO
+
+# TAMANHOS / POSIÇÕES
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# CORES
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# OUTROS
+MENU_OPTIONS = [
+                'NEW GAME [1P]',
+                'NEW GAME [2P] - COOPERATIVE',
+                'NEW GAME [2P] - COMPETITIVE',
+                'SCORE',
+                'EXIT'
+            ]
--- game_code/Menu.py
+++ game_code/Menu.py
@@ -1,6 +1,9 @@
 # Importações gerais
 import pygame
 
+# Importações do Python Package 'game_code' (MÓDULOS INTERNOS)
+from game_code.Consts import *
+
 class Menu:
 
     # CONSTRUTOR
@@ -24,8 +27,22 @@ class Menu:
         pygame.mixer_music.play(-1)
 
         while True:
+            
             # desenhando a imagem carregada (self.surface_img_place) no retângulo invisível 'self.rect'
             self.window.blit(source = self.surface_img_place, dest = self.rect)
+
+            # desenhando os textos do menu (são tratados como imagem) [CORES DEFINIDAS NO ARQUIVO 'Conts.py']
+            # [NOTA]: A ordem IMPORTA! Se você desenhar a imagem depois de escrever o texto, a imagem irá sobrepor o texto
+            # TÍTULO
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            # MENU
+            curr_y_pos = 180
+            for option in MENU_OPTIONS:
+
+                self.menu_text(15, option, COLOR_WHITE, ((WIN_WIDTH / 2), curr_y_pos))
+                curr_y_pos += 30
 
             # atualizando a tela para apresentar a imagem
             pygame.display.flip()
@@ -38,7 +55,10 @@ class Menu:
                     pygame.quit()   # Fecha a janela
                     quit()          # Encerra o pygame
 
-    # MÉTODO PARA APRESENTAÇÃO DE TEXTOS NO MENU
-    def menu_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+    # MÉTODO PARA APRESENTAÇÃO DE TEXTOS NO MENU (os textos são tratados como IMAGEM)
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
 
-        ...
+        text_font: pygame.font.Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: pygame.Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: pygame.Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
